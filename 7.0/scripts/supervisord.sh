146836fea684477d84062af7c1f0889fc1449187
Mark Hilton <mark@crunchgeek.com>
2017-11-06 14:36:13 -0500

    fixed some bugs

--- 7.0/scripts/supervisord.sh
+++ 7.0/scripts/supervisord.sh
@@ -3,7 +3,9 @@
 ### update PHP session handler
 if [ ! -z ${SUPERVISORD_PATH+x} ] && [ "$SUPERVISORD_PATH" != "" ]; then 
 	echo "env SUPERVISORD: including configurations from [ $SUPERVISORD_PATH ]"
-	echo "files = $SUPERVISORD_PATH" > /etc/supervisor/supervisord.conf
+
+	sed -i "/files = /d"                /etc/supervisor/supervisord.conf
+	echo "files = $SUPERVISORD_PATH" >> /etc/supervisor/supervisord.conf
 
 	/etc/init.d/supervisor start
 fi
