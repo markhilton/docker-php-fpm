9e17af9d5115e52f1da661b1d6fa8fb00cc0e5f2
Mark Hilton <nerd305@gmail.com>
2023-07-14 19:29:58 -0400

    newrelic update

--- 8.2/scripts/newrelic.sh
+++ 8.2/scripts/newrelic.sh
@@ -6,11 +6,6 @@ if [ ! -z ${NEWRELIC_LICENSE+x} ] && [ "$NEWRELIC_LICENSE" != "" ] ; then
     -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname=\"${NEWRELIC_APP_NAME}\"/" \
     $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini
     
-    nrsysmond-config --set license_key=$NEWRELIC_LICENSE
-    
-    service newrelic-sysmond start > /dev/null
-    
-    chmod u+rw,g+rw,o+rw /var/log/newrelic/newrelic-daemon.log
 else
     rm -f /usr/local/etc/php/conf.d/newrelic.ini
 fi
